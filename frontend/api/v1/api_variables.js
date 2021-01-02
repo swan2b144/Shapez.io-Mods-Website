8294c619885ba88faba8beff870cdc819be49343
DJ1TJOO <44841260+DJ1TJOO@users.noreply.github.com>
2021-01-02 20:26:03 +0100

    Delete api_variables.js

--- frontend/api/v1/api_variables.js
+++
@@ -1,5 +0,0 @@
-const secret = "Lwrbq96ohYfPL5965dUCBZn5DnuZBwl7";
-const clientID = "794256394441326632";
-const server = "http://localhost:3009/api/v1";
-const discordServerId = "776547645006938112";
-module.exports = { secret, clientID, server, discordServerId };
