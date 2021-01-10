4b590d784bef6e15f2cc9dfdd8e942e773198950
DJ1TJOO <t.ferb1@gmail.com>
2021-01-10 16:34:55 +0100

    Mod of the week

--- frontend/api/v1/database/mods.js
+++ frontend/api/v1/database/mods.js
@@ -19,15 +19,50 @@ const removeMod = (id, callback) => {
     db.remove("mods", id, callback);
 };
 
-const getModOTW = () => {
-    let best = { score: 0, mod: null };
-    const mods = db.getMods();
-    for (let i = 0; i < mods.length; i++) {
-        const mod = mods[i];
-        let rated = mod.likes + mod.downloads;
-        if (rated === 0) continue;
-        let score = rated / mod.seen;
-        if (score >= bestScore) best = { score: score, mod: mod };
-    }
+Date.prototype.getWeek = function() {
+    var date = new Date(this.getTime());
+    date.setHours(0, 0, 0, 0);
+    // Thursday in current week decides the year.
+    date.setDate(date.getDate() + 3 - ((date.getDay() + 6) % 7));
+    // January 4 is always in week 1.
+    var week1 = new Date(date.getFullYear(), 0, 4);
+    // Adjust to Thursday in week 1 and count number of weeks from date to week1.
+    return 1 + Math.round(((date.getTime() - week1.getTime()) / 86400000 - 3 + ((week1.getDay() + 6) % 7)) / 7);
+};
+
+const getModOTW = (callback) => {
+    mods((err, docs) => {
+        if (err) callback(err, null);
+        else if (docs.length > 0) {
+            let best = { score: 0, mod: null };
+            for (let i = 0; i < docs.length; i++) {
+                const mod = docs[i];
+                const now = new Date();
+                let likes = 0;
+                for (let j = 0; j < mod.likes.length; j++) {
+                    const likeData = mod.likes[j];
+                    if (likeData.getWeek() !== now.getWeek()) continue;
+                    likes++;
+                }
+                let downloads = 0;
+                for (let j = 0; j < mod.downloads.length; j++) {
+                    const downloadData = mod.downloads[j];
+                    if (downloadData.getWeek() !== now.getWeek()) continue;
+                    downloads++;
+                }
+                let seen = 0;
+                for (let j = 0; j < mod.seen.length; j++) {
+                    const seenData = mod.seen[j];
+                    if (seenData.getWeek() !== now.getWeek()) continue;
+                    seen++;
+                }
+                let rated = likes + downloads;
+                if (rated === 0) continue;
+                let score = rated / seen;
+                if (score >= bestScore) best = { score: score, mod: mod };
+            }
+            callback(null, best);
+        } else callback(null, null);
+    });
 };
 module.exports = { mods, findMod, addMod, editMod, removeMod, getModOTW };
