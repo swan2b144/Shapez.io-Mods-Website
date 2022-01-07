1ba94b8c6b6c3331a5fe509d4b7ce8a62adfe925
DJ1TJOO <t.ferb1@gmail.com>
2022-01-07 23:03:05 +0100

    Fixed language problems

--- api/v1/auth/auth.js
+++ api/v1/auth/auth.js
@@ -1,28 +1,13 @@
-const router = require("express").Router();
-const passport = require("passport");
-const languages = require("../languages");
+const router = require('express').Router();
+const passport = require('passport');
 
-router.get("/login", passport.authenticate("discord"));
-router.get(
-  "/discord/redirect",
-  passport.authenticate("discord"),
-  (req, res) => {
-    //Set language
-    let baseLanguage = Object.assign(
-      {},
-      languages.languages[languages.baseLanguage]
-    );
-    languages.matchDataRecursive(
-      baseLanguage,
-      languages.languages[req.user.settings.language]
-    );
-    req.language = baseLanguage;
-    res.redirect("/");
-  }
-);
-router.get("/logout", function (req, res) {
-  req.logout();
-  res.redirect("/");
+router.get('/login', passport.authenticate('discord'));
+router.get('/discord/redirect', passport.authenticate('discord'), (req, res) => {
+	res.redirect('/');
+});
+router.get('/logout', function (req, res) {
+	req.logout();
+	res.redirect('/');
 });
 
 module.exports = router;
--- dashboard/categories/settings.js
+++ dashboard/categories/settings.js
@@ -1,94 +1,94 @@
-const languages = require("../../api/v1/languages");
+const languages = require('../../api/v1/languages');
 module.exports = (req, res) => {
-  return {
-    id: "settings",
-    icon: "/static/images/settings.png",
-    invert: true,
-    title: req.language.dashboard.settings.title,
-    visible: true,
-    content: [
-      {
-        contentType: "setting",
-        type: "enum",
-        options: Object.keys(languages.languages),
-        getText: (languages, language, user) => (option) => {
-          return languages[option].name;
-        },
-        onChange: (languages, language, user) => (option) => {
-          let xhr = new XMLHttpRequest();
-          xhr.withCredentials = true;
-          xhr.onreadystatechange = function () {
-            if (xhr.readyState == XMLHttpRequest.DONE) {
-              window.location.reload();
-            }
-          };
-          xhr.open(`PATCH`, `/api/v1/database/users/`, true);
-          xhr.setRequestHeader(`Content-Type`, `application/json`);
-          xhr.send(
-            JSON.stringify({
-              [`settings.language`]: option,
-            })
-          );
-        },
-        value: req.user.settings.language,
-        title: req.language.dashboard.settings.content.language.title,
-        desc: req.language.dashboard.settings.content.language.description,
-      },
-      // {
-      //     contentType: "setting",
-      //     type: "range",
-      //     min: 0,
-      //     max: 200,
-      //     step: 2,
-      //     getText: (value) => Math.floor(value) + "%",
-      //     onChange: (value) => {
-      //         console.log(value);
-      //     },
-      //     value: 6,
-      //     title: "Geluidsvolume",
-      //     desc: "Stel het volume voor geluidseffecten in.",
-      // },
-      {
-        contentType: "setting",
-        type: "boolean",
-        onChange: (languages, language, user) => (value) => {
-          let xhr = new XMLHttpRequest();
-          xhr.withCredentials = true;
-          xhr.open(`PATCH`, `/api/v1/database/users/`, true);
-          xhr.setRequestHeader(`Content-Type`, `application/json`);
-          xhr.send(
-            JSON.stringify({
-              [`settings.publicTag`]: value,
-            })
-          );
-        },
-        value: req.user.settings.publicTag,
-        title: req.language.dashboard.settings.content.publicTag.title,
-        desc: req.language.dashboard.settings.content.publicTag.description,
-      },
-      {
-        title: "Profile",
-        contentType: "form",
-        content: [
-          {
-            type: "page",
-            id: "profile-page",
-            classes: [],
-            value: req.user.description,
-            onChange: (languages, language, user) => (value) => {
-              let xhr = new XMLHttpRequest();
-              xhr.withCredentials = true;
-              xhr.open(`PATCH`, `/api/v1/database/users/`, true);
-              xhr.setRequestHeader(`Content-Type`, `application/json`);
-              xhr.send(
-                JSON.stringify({
-                  [`description`]: value,
-                })
-              );
-            },
-          },
-        ],
-      },
-    ],
-  };
+	return {
+		id: 'settings',
+		icon: '/static/images/settings.png',
+		invert: true,
+		title: req.language.dashboard.settings.title,
+		visible: true,
+		content: [
+			{
+				contentType: 'setting',
+				type: 'enum',
+				options: languages.languages,
+				getText: (languages, language, user) => (option) => {
+					return languages[option].name;
+				},
+				onChange: (languages, language, user) => (option) => {
+					let xhr = new XMLHttpRequest();
+					xhr.withCredentials = true;
+					xhr.onreadystatechange = function () {
+						if (xhr.readyState == XMLHttpRequest.DONE) {
+							window.location.reload();
+						}
+					};
+					xhr.open(`PATCH`, `/api/v1/database/users/`, true);
+					xhr.setRequestHeader(`Content-Type`, `application/json`);
+					xhr.send(
+						JSON.stringify({
+							[`settings.language`]: option,
+						}),
+					);
+				},
+				value: req.user.settings.language,
+				title: req.language.dashboard.settings.content.language.title,
+				desc: req.language.dashboard.settings.content.language.description,
+			},
+			// {
+			//     contentType: "setting",
+			//     type: "range",
+			//     min: 0,
+			//     max: 200,
+			//     step: 2,
+			//     getText: (value) => Math.floor(value) + "%",
+			//     onChange: (value) => {
+			//         console.log(value);
+			//     },
+			//     value: 6,
+			//     title: "Geluidsvolume",
+			//     desc: "Stel het volume voor geluidseffecten in.",
+			// },
+			{
+				contentType: 'setting',
+				type: 'boolean',
+				onChange: (languages, language, user) => (value) => {
+					let xhr = new XMLHttpRequest();
+					xhr.withCredentials = true;
+					xhr.open(`PATCH`, `/api/v1/database/users/`, true);
+					xhr.setRequestHeader(`Content-Type`, `application/json`);
+					xhr.send(
+						JSON.stringify({
+							[`settings.publicTag`]: value,
+						}),
+					);
+				},
+				value: req.user.settings.publicTag,
+				title: req.language.dashboard.settings.content.publicTag.title,
+				desc: req.language.dashboard.settings.content.publicTag.description,
+			},
+			{
+				title: 'Profile',
+				contentType: 'form',
+				content: [
+					{
+						type: 'page',
+						id: 'profile-page',
+						classes: [],
+						value: req.user.description,
+						onChange: (languages, language, user) => (value) => {
+							let xhr = new XMLHttpRequest();
+							xhr.withCredentials = true;
+							xhr.open(`PATCH`, `/api/v1/database/users/`, true);
+							xhr.setRequestHeader(`Content-Type`, `application/json`);
+							xhr.send(
+								JSON.stringify({
+									[`description`]: value,
+								}),
+							);
+						},
+					},
+				],
+			},
+		],
+	};
 };
